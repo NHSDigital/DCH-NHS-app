5b72bcfa59c453b312ab9c0cd395326c28f2e787
jamespallister2 <james.pallister2@nhs.net>
2026-01-29 09:17:02 +0000

    Luke tidy up wednesday evening

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -9,7 +9,7 @@ module.exports = {
   "linkedProfileFirstName": "Teddy",
   "linkedProfileLastName": "Goodall",
   "nhsNumber": "123 456 7899",
-    "linkedProfileAge": "3 weeks 2 days",
+    "linkedProfileAge": "0 years 3 weeks 2 days old",
 
 
   // Set user details
