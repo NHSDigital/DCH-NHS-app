419c526845a5d7ba93d13def0862fbdcbec8f90f
Ash Finnegan <aislinn.finnegan1@nhs.net>
2026-07-23 10:03:00 +0100

    refactor: update routes

--- app/routes.js
+++ app/routes.js
@@ -30,7 +30,6 @@
         }
     })
 
-
     // Catch the redirect specifically from the enter-your-name form
     router.post('/v5.5/pages/opt-in/enter-your-name-post', function (req, res) {
         const knowsNhsNumber = req.session.data['do-you-know-nhs-number'];
@@ -51,6 +50,47 @@
         } else {
             // Validation fallback if they select nothing
             res.render('v5.5/pages/opt-in/do-you-know-nhs-number', {
+                error: true
+            })
+        }
+    })
+
+    // V6 Routes
+
+    router.post('/v6/pages/opt-in/preferences-1-post', function (req, res) {
+        const input = req.session.data['opt-in-preferences']
+
+        if (input === 'receive') {
+            res.redirect('/v6/pages/opt-in/confirmation-saved-receive')
+        } else if (input === 'dont-receive') {
+            res.redirect('/v6/pages/opt-in/confirmation-saved-dont-receive')
+        } else {
+            // Validation fallback if they select nothing
+            res.render('v6/pages/opt-in/preferences-1', {
+                error: true
+            })
+        }
+    })
+
+    router.post('/v6/pages/opt-in/enter-your-name-post', function (req, res) {
+        const knowsNhsNumber = req.session.data['do-you-know-nhs-number'];
+        if (knowsNhsNumber === 'yes') {
+             res.redirect('/v6/pages/opt-in/nhs-check-details');
+        } else {
+             res.redirect('/v6/pages/opt-in/enter-date-of-birth');
+        }
+    })
+
+    router.post('/v6/pages/opt-in/do-you-know-nhs-number', function (req, res) {
+        const input = req.session.data['do-you-know-nhs-number']
+
+        if (input === 'yes') {
+            res.redirect('/v6/pages/opt-in/enter-your-nhs-number')
+        } else if (input === 'no') {
+            res.redirect('/v6/pages/opt-in/enter-your-name')
+        } else {
+            // Validation fallback if they select nothing
+            res.render('v6/pages/opt-in/do-you-know-nhs-number', {
                 error: true
             })
         }
